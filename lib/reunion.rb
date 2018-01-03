f328f97d76006c6677ef9ec8d590b4229df5acec
Ellen Cornelius <ecorneli@iwu.edu>
2018-01-03 10:25:01 -0700

    Add reunion class and test has activities

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,2 +1,12 @@
+require './lib/activity'
 class Reunion
+
+attr_reader :location,
+            :activities
+
+  def initialize(location)
+    @location = location
+    @activities = Activity.new("hey")
+  end
+
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -1,7 +1,20 @@
 require 'minitest'
 require 'minitest/test'
 require 'minitest/autorun'
+require 'minitest/pride'
 require './lib/reunion'
 
 class ReunionTest < Minitest::Test
+
+  def test_reunion_has_location
+    reunion = Reunion.new("Camp Echo")
+
+    assert_equal "Camp Echo", reunion.location
+  end
+
+  def test_reunion_has_activities
+    reunion = Reunion.new("Camp Echo")
+
+    assert_instance_of Activity, reunion.activities
+  end
 end
